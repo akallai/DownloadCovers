a8e03fd138c6fbd887baa85bd9d3543fc6ed3b0b
Unknown <akallai@uni-potsdam.de>
2017-06-22 13:07:57 +0200

    working skript

--- Download Cover.py
+++ Download Cover.py
@@ -3,6 +3,7 @@ import urllib.request
 import os
 
 link=r"http://www.imdb.com/"
+errorList=[]
 
 def getCode(title, url=r"http://www.imdb.com/"):
    
@@ -19,7 +20,7 @@ def iterateFolder(folder):
     for subdir, dirs, files in os.walk(rootdir):
         #print (os.path.join(subdir, file))
         makeLine("nächster Film")
-        if os.path.isfile(os.path.join(subdir, "folder.jpg")):
+        if os.path.isfile(os.path.join(subdir, "folder.jpg")):                              #Cover bereits vorhanden
             print("") 
             path = str(subdir)
             reversePath= path[::-1]
@@ -28,7 +29,7 @@ def iterateFolder(folder):
             foldername=path[stelle1:]
             print("%s hat bereits ein Cover..." % foldername)
         else:
-            if not ersterDurchlauf:
+            if not ersterDurchlauf:                                                         #Cover nicht vorhanden
                 path = str(subdir)
                 reversePath= path[::-1]
                 stelle1=reversePath.find("\\")
@@ -60,14 +61,14 @@ def DownloadPicture(link, where):
         print("Cover konnte nicht heruntergeladen werden!")
 
 def FindTitle(words):
-    suchbegriffe=words#überflüssig
+    suchbegriffe=words
     #suche für imdb angepasst
     suchbegriffe=str.replace(suchbegriffe,"&", "%26")
     suchbegriffe=str.replace(suchbegriffe," ", "+")
     website="http://www.imdb.com/find?ref_=nv_sr_fn&q=%s&s=tt" % urllib.parse.quote(suchbegriffe)
     t = urllib.request.urlopen(website)
     inhalt=str(t.read())
-    txt = open(r"C:\Users\Antonio.Kallai\Desktop\inhalt.txt", 'w')
+    txt = open(r"C:\Users\Antonio.Kallai\Desktop\inhalt.txt", 'a')
     txt.write(inhalt)
     txt.close()
     stelle = inhalt.find("/title/")
@@ -99,9 +100,32 @@ def FindTitle(words):
     if isnofilm!=-1:
         answer=answer[isnofilm+7:]
     print("Cover gefunden für: %s"%answer)
+    if sameYear(words, answer)==False:
+        errorList.append(words)
+        f=open(errorListPlace+r"\fehlerliste.txt",mode="a")
+        f.write(words+"\n")
+        f.close
+        print("Für %s wird ein falsches Cover heruntergeladen!!!" % words)
     if answer == r"IMDb - Movies, TV and Celebrities":
         print("Titel nicht gefunden")
         return ""
+    
     return linkcover
-
-iterateFolder(r"C:\Users\Antonio.Kallai\Desktop\New folder")
+def sameYear(title1, title2):
+    stelle=title1.find("(")
+    title1=title1[stelle:stelle+6]
+    stelle=title2.find("(")
+    title2=title2[stelle:stelle+6]
+    if title1==title2:
+        return True
+    return False
+errorListPlace=input("An welchem Pfad soll Fehlerliste angelegt werden: ")
+if errorListPlace=="test":
+    errorListPlace=r"C:\Users\Antonio.Kallai\Desktop"
+filmfolder=input("Bitte gib den Filmordner an: ")
+if filmfolder=="test":
+    filmfolder=r"C:\Users\Antonio.Kallai\Desktop\New folder"
+iterateFolder(filmfolder)
+print("\n\nListe von Fehlerhaften Covers, da die Jahreszahl nicht übereinstimmt oder kein Cover gefunden wurde")
+for element in errorList:
+    print(element+"\n")
